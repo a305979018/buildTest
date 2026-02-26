fe22c4accc515e21a1576e3d5633e9b99b72e876
夜冰 <1583768914@qq.com>
2026-02-26 14:10:14 +0800

    build文件

--- build-ci.bat
+++ build-ci.bat
@@ -1,9 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
+
 set MODULE=%~1
 if "%MODULE%"=="" set MODULE=GP_maong
 set BUILD_TYPE=%~2
 if "%BUILD_TYPE%"=="" set BUILD_TYPE=Debug
-if not exist gradlew.bat exit /b 1
-call gradlew.bat :%MODULE%:clean :%MODULE%:assemble%BUILD_TYPE% -x test
+
+:: 这里直接调用本地 Gradle
+set GRADLE_HOME=D:\andorid_tools\gradle-8.14-rc-2
+set PATH=%GRADLE_HOME%\bin;%PATH%
+
+gradle :%MODULE%:clean :%MODULE%:assemble%BUILD_TYPE% -x test
 exit /b %ERRORLEVEL%
